a58099098b720d1333c6d1da37c8f420a1b66e39
snehal132 <mahajansnehal132@gmail.com>
2019-05-29 16:38:29 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'JasmineKarmaExample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('JasmineKarmaExample    ');
+    expect(app.title).toEqual('JasmineKarmaExample');
   });
 
   it(`Test case 2`, () => {
